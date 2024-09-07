3f84c868fe747c2110b31a8f08b125f4adb97653
Alden <aldenjbeck1@gmail.com>
2024-09-07 00:17:39 -0400

    updated

--- lua/Alden/plugins/icons.lua
+++ lua/Alden/plugins/icons.lua
@@ -1,12 +1,15 @@
 return {
-    "prichrd/netrw.nvim",
-    opts = {
-        icons = {
-            symlink = '', -- Symlink icon (directory and file)
-            directory = '', -- Directory icon
-            file = '', -- File icon
-        },
-        use_devicons = true, -- Uses nvim-web-devicons if true, otherwise use the file icon specified above
-        mappings = {}, -- Custom key mappings
+    -- { "DaikyXendo/nvim-material-icon" },
+    {
+        "prichrd/netrw.nvim",
+        opts = {
+            icons = {
+                symlink = '', -- Symlink icon (directory and file)
+                directory = '', -- Directory icon
+                file = '', -- File icon
+            },
+            use_devicons = true, -- Uses nvim-web-devicons if true, otherwise use the file icon specified above
+            mappings = {}, -- Custom key mappings
+        }
     }
 }
