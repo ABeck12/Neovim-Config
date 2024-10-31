f9c0f90cc88d9bdfe717fcf28ef512e627afb752
Alden Beck <aldenjbeck1@gmail.com>
2024-10-30 22:38:54 -0400

    Updated note taking plugins

--- lua/Alden/plugins/note_taking.lua
+++ lua/Alden/plugins/note_taking.lua
@@ -1,5 +1,16 @@
 return {
     { "dhruvasagar/vim-table-mode" },
     { "opdavies/toggle-checkbox.nvim" },
-    vim.keymap.set("n", "<leader>tc", ":lua require('toggle-checkbox').toggle()<CR>")
+    vim.keymap.set("n", "<leader>tc", ":lua require('toggle-checkbox').toggle()<CR>"),
+
+    --{
+    --    'MeanderingProgrammer/render-markdown.nvim',
+    --    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    --    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    --    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    --    ---@module 'render-markdown'
+    --    ---@type render.md.UserConfig
+    --    opts = {},
+    --},
+
 }
