617b20eede392f39e27041ca25f8a6b2ac847999
Alden Beck <aldenjbeck1@gmail.com>
2025-04-13 17:39:53 -0400

    Removed unused plugins

--- lua/Alden/plugins/lsp.lua
+++ lua/Alden/plugins/lsp.lua
@@ -1,6 +1,7 @@
 local utils = require("Alden.utils")
 
 return {
+    { "tikhomirov/vim-glsl" }, {
     "neovim/nvim-lspconfig",
 
     event = {
@@ -101,7 +102,8 @@ return {
                 norrmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
                 norrmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
                 norrmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-                norrmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+                norrmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols,
+                    '[W]orkspace [S]ymbols')
                 norrmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
                 norrmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
                 norrmap('K', vim.lsp.buf.hover, 'Hover Documentation')
@@ -147,4 +149,4 @@ return {
             end
         })
     end
-}
+} }
